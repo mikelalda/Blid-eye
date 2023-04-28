8854d4a092c4c61f3ae72be22b871bb44ff445bf
Mikel Aldalur <mikelalda13@gmail.com>
2023-04-28 09:18:12 +0200

    Pasando a metodo sencillo

---
+++ arduino/esp_wifi/esp_wifi.ino
@@ -0,0 +1,83 @@
+
+/* This example is written for Nodemcu Modules */
+
+#include "ESP_Wahaj.h" // importing our library
+
+int left = 16; // pin D0
+int middle = 5; // pin D1
+int right = 4; // pin D2
+
+String path = "nothing";
+void setup(){
+  Serial.begin(9600);
+  start("dlink-FB82","oikzd47889");  // Wifi details connect to
+  // initialize GPIO 5 as an output
+
+  pinMode(left, OUTPUT);
+  pinMode(middle, OUTPUT);
+  pinMode(right, OUTPUT);
+}
+
+void loop(){
+  //waitUntilNewReq();  //Waits until a new request from python come
+
+  if(CheckNewReq() == 1)
+  {
+    //Serial.println("new request");
+    if (getPath()=="/1"){
+      digitalWrite(left, HIGH); // turn the LED on
+      digitalWrite(middle, LOW); // turn the LED on
+      digitalWrite(right, LOW); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("1");
+    }
+    else if (getPath()=="/2"){
+      digitalWrite(left, HIGH); // turn the LED on
+      digitalWrite(middle, HIGH); // turn the LED on
+      digitalWrite(right, LOW); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("2");
+    }
+    else if (getPath()=="/3"){
+      digitalWrite(left, LOW); // turn the LED on
+      digitalWrite(middle, HIGH); // turn the LED on
+      digitalWrite(right, LOW); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("3");
+    }
+    else if (getPath()=="/4"){
+      digitalWrite(left, LOW); // turn the LED on
+      digitalWrite(middle, HIGH); // turn the LED on
+      digitalWrite(right, HIGH); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("4");
+    }
+    else if (getPath()=="/5"){
+      digitalWrite(left, LOW); // turn the LED on
+      digitalWrite(middle, LOW); // turn the LED on
+      digitalWrite(right, HIGH); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("5");
+    }
+    else if (getPath()=="/6"){
+      digitalWrite(left, HIGH); // turn the LED on
+      digitalWrite(middle, HIGH); // turn the LED on
+      digitalWrite(right, HIGH); // turn the LED on
+      delay(10); // wait for 10ms
+    returnThisStr("6");
+    }
+
+    else        //here we receive data. You can receive pwm255 and the decode it to 255 and also get more variables like this
+    {
+      path = getPath();
+      Serial.println(path);
+    }
+    
+  }
+  
+//Serial.println("tesiting....");
+//if(pwm == 255) Serial.println("highhhhh");
+//if(pwm == 0) Serial.println("lowwwwsssh");
+//analogWrite(led,pwm);
+  
+}
--- main.py
+++ main.py
@@ -1,44 +1,80 @@
-import additionals.inference as infer
-import additionals.globals as gv
 import argparse
 import socket
-import keyboard
-    
-def main(model_path, ip, port):
-    global inference
+import jetson_inference
+import jetson_utils
+import additionals.globals as gv
 
-    inference = infer(model_path)
+import urllib.request
+import http
+
+base = "http://192.168.10.19/"
+
+
+def transfer(my_url):   #use to send and receive data
+    try:
+        n = urllib.request.urlopen(base + my_url).read()
+        n = n.decode("utf-8")
+        return n
+
+    except http.client.HTTPException as e:
+        return e
+
+net = jetson_inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = jetson_utils.videoSource("csi://0")      # '/dev/video0' for V4L2
+display = jetson_utils.videoOutput("display://0") # 'my_video.mp4' for file
+render_img = False
+
+def main(ip, port):
 
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect((ip,port))
 
     while True:
-        boxes, scores, classes = inference.next()
-        for i in classes:
-            if classes[i] in gv.DETECTIONS:
+        img = camera.Capture()
+        detections = net.Detect(img)
+        if render_img:
+            display.Render(img)
+            display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+        for detection in detections:
+            class_id = detection.ClassID
+            x1 = detection.Left   
+            y1 = detection.Top    
+            x2 = detection.Right  
+            y2 = detection.Bottom 
+            if class_id in gv.DETECTIONS:
                 '''
                 Suponiendo que boxes[0] is x.min, boxes[1] is y.min, boxes[2] is width and boxes[3] is height
                 '''
                 # Si esta en la parte izquierda de la imagen
-                if gv.X_L_MIN < boxes[i][0] < gv.X_L_MAX and  gv.X_L_MIN < boxes[i][0] + boxes[i][2] < gv.X_L_MAX:
-                    conn.send('1')
+                if gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_L_MIN < x2 < gv.X_L_MAX:
+                    check = transfer("1")
+                    if check != "1":
+                        print("An error ocurred while transfer")
                 # Si esta en la parte izquierda-central de la imagen
-                elif gv.X_L_MIN < boxes[i][0] < gv.X_L_MAX and  gv.X_M_MIN < boxes[i][0] + boxes[i][2] < gv.X_M_MAX:
-                    conn.send('2')
+                elif gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
+                    check = transfer('2')
+                    if check != "2":
+                        print("An error ocurred while transfer")
                 # Si esta en la parte central de la imagen
-                elif gv.X_M_MIN < boxes[i][0] < gv.X_M_MAX and  gv.X_M_MIN < boxes[i][0] + boxes[i][2] < gv.X_M_MAX:
-                    conn.send('3')
+                elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
+                    check = transfer('3')
+                    if check != "3":
+                        print("An error ocurred while transfer")
                 # Si esta en la parte central-derecha de la imagen
-                elif gv.X_M_MIN < boxes[i][0] < gv.X_M_MAX and  gv.X_R_MIN < boxes[i][0] + boxes[i][2] < gv.X_R_MAX:
-                    conn.send('4')
+                elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
+                    check = transfer('4')
+                    if check != "4":
+                        print("An error ocurred while transfer")
                 # Si esta en la parte derecha de la imagen
-                elif gv.X_R_MIN < boxes[i][0] < gv.X_R_MAX and  gv.X_R_MIN < boxes[i][0] + boxes[i][2] < gv.X_R_MAX:
-                    conn.send('5')
+                elif gv.X_R_MIN < x1 < gv.X_R_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
+                    check = transfer('5')
+                    if check != "5":
+                        print("An error ocurred while transfer")
                 # Si esta de izquierda a derecha ocupando casi toda la imagen
-                elif gv.X_L_MIN < boxes[i][0] < gv.X_L_MAX/2 and  gv.X_R_MIN < boxes[i][0] + boxes[i][2] < (gv.X_R_MAX - (gv.X_R_MAX-gv.X_R_MIN)/2):
-                    conn.send('6')
-        if keyboard.read_key() == "q" or keyboard.read_key() == "Q":
-            break
+                elif gv.X_L_MIN < x1 < gv.X_L_MAX/2 and  gv.X_R_MIN < x2 < (gv.X_R_MAX - (gv.X_R_MAX-gv.X_R_MIN)/2):
+                    check = transfer('6')
+                    if check != "6":
+                        print("An error ocurred while transfer")
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create an alarm')
@@ -50,4 +86,4 @@ if __name__ == '__main__':
                         help='the path to model')
     args = parser.parse_args()
 
-    main()
+    main(args.ip, args.port)
