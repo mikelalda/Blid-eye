7493b8b3946164c348e7ae4c44f58aa3f202acf2
Mikel Aldalur <mikelalda13@gmail.com>
2023-07-20 18:25:04 +0200

    Fix error handle

--- main.py
+++ main.py
@@ -38,68 +38,71 @@ names = ['unlabeled', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus
 def main():
 
     while True:
-        img = camera.Capture()
-        height, width, channels = img.shape
-        detections = net.Detect(img)
-        if render_img:
-            display.Render(img)
-            display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-        for detection in detections:
-            class_id = names[detection.ClassID]
-            x1 = detection.Left/width 
-            y1 = detection.Top/height
-            x2 = detection.Right/width
-            y2 = detection.Bottom/height
-            if class_id in gv.DETECTIONS:
-                '''
-                Suponiendo que boxes[0] is x.min, boxes[1] is y.min, boxes[2] is width and boxes[3] is height
-                '''
-                # Si esta en la parte izquierda de la imagen
-                if gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_L_MIN < x2 < gv.X_L_MAX:
-                    print(1)
-                    check = transfer("1")
-                    if check != "1":
-                        print("An error ocurred while transfer")
-                # Si esta en la parte izquierda-central de la imagen
-                elif gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
-                    print(2)
-                    check = transfer('2')
-                    if check != "2":
-                        print("An error ocurred while transfer")
-                # Si esta en la parte central de la imagen
-                elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
-                    print(3)
-                    check = transfer('3')
-                    if check != "3":
-                        print("An error ocurred while transfer")
-                # Si esta en la parte central-derecha de la imagen
-                elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
-                    print(4)
-                    check = transfer('4')
-                    if check != "4":
-                        print("An error ocurred while transfer")
-                # Si esta en la parte derecha de la imagen
-                elif gv.X_R_MIN < x1 < gv.X_R_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
-                    print(5)
-                    check = transfer('5')
-                    if check != "5":
-                        print("An error ocurred while transfer")
-                # Si esta de izquierda a derecha ocupando casi toda la imagen
-                elif gv.X_L_MIN < x1 < gv.X_L_MAX/2 and  gv.X_R_MIN < x2 < (gv.X_R_MAX - (gv.X_R_MAX-gv.X_R_MIN)/2):
-                    print(6)
-                    check = transfer('6')
-                    if check != "6":
-                        print("An error ocurred while transfer")
+        try:
+            img = camera.Capture()
+            height, width, channels = img.shape
+            detections = net.Detect(img)
+            if render_img:
+                display.Render(img)
+                display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+            for detection in detections:
+                class_id = names[detection.ClassID]
+                x1 = detection.Left/width 
+                y1 = detection.Top/height
+                x2 = detection.Right/width
+                y2 = detection.Bottom/height
+                if class_id in gv.DETECTIONS:
+                    '''
+                    Suponiendo que boxes[0] is x.min, boxes[1] is y.min, boxes[2] is width and boxes[3] is height
+                    '''
+                    # Si esta en la parte izquierda de la imagen
+                    if gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_L_MIN < x2 < gv.X_L_MAX:
+                        print(1)
+                        check = transfer("1")
+                        if check != "1":
+                            print("An error ocurred while transfer")
+                    # Si esta en la parte izquierda-central de la imagen
+                    elif gv.X_L_MIN < x1 < gv.X_L_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
+                        print(2)
+                        check = transfer('2')
+                        if check != "2":
+                            print("An error ocurred while transfer")
+                    # Si esta en la parte central de la imagen
+                    elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_M_MIN < x2 < gv.X_M_MAX:
+                        print(3)
+                        check = transfer('3')
+                        if check != "3":
+                            print("An error ocurred while transfer")
+                    # Si esta en la parte central-derecha de la imagen
+                    elif gv.X_M_MIN < x1 < gv.X_M_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
+                        print(4)
+                        check = transfer('4')
+                        if check != "4":
+                            print("An error ocurred while transfer")
+                    # Si esta en la parte derecha de la imagen
+                    elif gv.X_R_MIN < x1 < gv.X_R_MAX and  gv.X_R_MIN < x2 < gv.X_R_MAX:
+                        print(5)
+                        check = transfer('5')
+                        if check != "5":
+                            print("An error ocurred while transfer")
+                    # Si esta de izquierda a derecha ocupando casi toda la imagen
+                    elif gv.X_L_MIN < x1 < gv.X_L_MAX/2 and  gv.X_R_MIN < x2 < (gv.X_R_MAX - (gv.X_R_MAX-gv.X_R_MIN)/2):
+                        print(6)
+                        check = transfer('6')
+                        if check != "6":
+                            print("An error ocurred while transfer")
+                    else:
+                        print(0)
+                        check = transfer('0')
+                        if check != "0":
+                            print("An error ocurred while transfer")
                 else:
                     print(0)
                     check = transfer('0')
                     if check != "0":
                         print("An error ocurred while transfer")
-            else:
-                print(0)
-                check = transfer('0')
-                if check != "0":
-                    print("An error ocurred while transfer")
     
+        except:
+            None
 if __name__ == '__main__':
     main()
